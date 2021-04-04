6eea275dc615c13de14cbf814fc9aea26dd8e059
Djabrail Gadzhimuradov <djabrail92@gmail.com>
2021-04-04 03:25:58 +0300

    add file now.json 3

--- pages/_title_slug.vue
+++ pages/_title_slug.vue
@@ -1,12 +1,11 @@
 <template>
   <section>
-    <h2>Posts</h2>
     <article class="my-8">
       <div class="text-gray-600 font-bold text-sm tracking-wide">
         <a v-for="(tag, key) in post.tags" :key="key" :href="'/category/'+tag" class="ml-1">{{ tag }}</a>
       </div>
       <h1 class="mt-2 text-3xl font-bold">
-        {{ post.title }}
+        {{ post }}
       </h1>
       <div class="mt-4 markdown" v-html="post.excerpt + '\n' + post.content">
       </div>
@@ -20,15 +19,17 @@ export default {
     if (payload) {
       return { post: payload }
     } else {
-      let { data } = await app.$axios.post(`https://cms.brodigital.ru/api/collections/get/posts?id=${params.title_slug}&token=account-20a9d2f75140ba5a0eefec892eef6d`,
+      let { data } = await app.$axios.post('https://cms.brodigital.ru/api/collections/get/posts?id=${params.title_slug}&token=account-20a9d2f75140ba5a0eefec892eef6d',
       JSON.stringify({
-          filter: { published: true },
+          filter: { published: true, title_slug: params.title_slug },
           sort: {_created:-1},
           populate: 1
         }),
       {
         headers: { 'Content-Type': 'application/json' }
       })
+
+      console.log(data);
 
       if (!data.entries[0]) {
         return error({ message: '404 Page not found', statusCode: 404 })
