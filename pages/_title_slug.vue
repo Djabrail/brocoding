4dd3aa1b801b8a6219661a83129be1d12667d992
Djabrail Gadzhimuradov <djabrail92@gmail.com>
2021-04-04 03:42:51 +0300

    add file now.json 5

--- pages/_title_slug.vue
+++ pages/_title_slug.vue
@@ -5,7 +5,7 @@
         <a v-for="(tag, key) in post.tags" :key="key" :href="'/category/'+tag" class="ml-1">{{ tag }}</a>
       </div>
       <h1 class="mt-2 text-3xl font-bold">
-        {{ post }}
+        {{ post.title }}
       </h1>
       <div class="mt-4 markdown" v-html="post.excerpt + '\n' + post.content">
       </div>
@@ -17,7 +17,7 @@
 export default {
   async asyncData({ $axios, params }) {
     const { data } = await $axios.get(`https://cms.brodigital.ru/api/collections/get/posts?id=${params.title_slug}&token=account-20a9d2f75140ba5a0eefec892eef6d`);
-    return { post: data.entries }
+    return { post: data.entries[0] }
   }
 }
 </script>
