d74a3d9871912962ad46d8a69cb9e4dcf484e211
Djabrail Gadzhimuradov <djabrail92@gmail.com>
2021-04-04 03:50:19 +0300

    add file now.json 5

--- pages/_title_slug.vue
+++ pages/_title_slug.vue
@@ -17,7 +17,7 @@
 <script>
 export default {
   async asyncData ({ app, params }) {
-    const { data } = await app.$axios.post(`https://cms.brodigital.ru/api/collections/get/posts?token=account-20a9d2f75140ba5a0eefec892eef6d`,
+    const { data } = await app.$axios.post(`${process.env.POSTS_URL}`,
     JSON.stringify({
         filter: { published: true, title_slug: params.title_slug },
         sort: {_created:-1},
